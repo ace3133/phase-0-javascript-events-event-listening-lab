f891c4ee4b5d5c3e328468d2081ab69f97c85c21
Hass <hassmroueh@gmail.com>
2024-01-17 22:26:18 -0500

    javascript events listing lab complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
-}
+    function addingEventListener() {
+      // Get the button element by its ID
+      var button = document.getElementById("button");
+  
+      // Add an event listener to the button
+      button.addEventListener("click", function() {
+        // Your code to be executed when the button is clicked
+        alert("Button clicked!");
+      });
+    }
+  
+    // Call the addingEventListener function
+    addingEventListener(); 
+  
+  
